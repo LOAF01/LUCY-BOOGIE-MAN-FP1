9f76f084c6a251778ed63f3619436dcbda64e524
ion <delion.dev@gmail.com>
2025-01-09 23:46:41 +0900

    [Feat] cursor 커스텀

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import './App.css';
 
 function App() {
   const artistRef = useRef(null);
+  const mouseRef = useRef(null);
+  const mouseChildRef = useRef(null);
   
   const [menu, setMenu] = useState(null);
   const [displayedText, setDisplayedText] = useState([]);
@@ -33,25 +35,60 @@ function App() {
     ],
   };
 
-function toggleMenu(p) {
-  if (menu === p) {
-    setFadeOut(true);
-    setTimeout(() => {
-      setMenu(null);
-      setDisplayedText([]);
-      setFadeOut(false);
-    }, 800);
-  } else {
-    setFadeOut(true);
-    setTimeout(() => {
-      setCurrentLine(0);
-      setIndex(0);
-      setMenu(p);
-      setDisplayedText([]);
-      setFadeOut(false);
-    }, 800);
-  }
-}
+  let cursorX = 0, cursorY = 0;
+  let scale = 1;
+  let color = '#e50000';
+  let stage = '';
+  
+  function mousemove(e) {
+    cursorX = e.pageX - mouseRef.current.offsetWidth / 2;
+    cursorY = e.pageY - mouseRef.current.offsetHeight / 2;
+    mouseRef.current.style.transform = 
+      `translate3d(${cursorX}px, ${cursorY}px, 0)`;
+
+    switch(e.target.getAttribute('data-cursor')) {
+      case 'title':
+        if (stage === 'title') return;
+        scale = 4;
+        color = '#e50000';
+        stage = 'title';
+        break;
+      case 'liryc':
+        if (stage === 'liryc') return;
+        scale = 4;
+        color = '#00fbff';
+        stage = 'liryc';
+        break;
+      default:
+        if (stage === '') return;
+        scale = 1;
+        color = '#e50000';
+        stage = '';
+        break;
+    }
+    mouseChildRef.current.style.setProperty('--cursor-scale', scale);
+    mouseChildRef.current.style.setProperty('--cursor-clr', color);
+  };
+
+  function toggleMenu(p) {
+    if (menu === p) {
+      setFadeOut(true);
+      setTimeout(() => {
+        setMenu(null);
+        setDisplayedText([]);
+        setFadeOut(false);
+      }, 800);
+    } else {
+      setFadeOut(true);
+      setTimeout(() => {
+        setCurrentLine(0);
+        setIndex(0);
+        setMenu(p);
+        setDisplayedText([]);
+        setFadeOut(false);
+      }, 800);
+    }
+  };
 
   function updatingText(newWord) {
     const updatedLines = [...displayedText];
@@ -81,62 +118,68 @@ function toggleMenu(p) {
   }, [menu, currentLine, index]);
 
   useEffect(() => {
-    const ref = artistRef.current;
-    if (!ref) return;
+    if (!artistRef.current || !mouseRef.current) return;
+
+    document.addEventListener('mousemove', mousemove);
 
     let cnt = 0;
 
-    const interval = setInterval(() => {
-      // ref
+    const glitchInterval = setInterval(() => {
+      // artistRef.current
       const skew = Math.random() * 10 - 5;
-      // ref::before
+      // artistRef.current::before
       const top1 = Math.random() * 100;
       const btm1 = Math.random() * 100;
-      // ref::after 
+      // artistRef.current::after 
       const top2 = Math.random() * 100;
       const btm2 = Math.random() * 100;
 
-      ref.style.setProperty('--skew', `${skew}deg`);
-      ref.style.setProperty('--t1', `${top1}%`);
-      ref.style.setProperty('--b1', `${btm1}%`);
-      ref.style.setProperty('--t2', `${top2}%`);
-      ref.style.setProperty('--b2', `${btm2}%`);
-      ref.style.setProperty('--scale', '1');
+      artistRef.current.style.setProperty('--skew', `${skew}deg`);
+      artistRef.current.style.setProperty('--t1', `${top1}%`);
+      artistRef.current.style.setProperty('--b1', `${btm1}%`);
+      artistRef.current.style.setProperty('--t2', `${top2}%`);
+      artistRef.current.style.setProperty('--b2', `${btm2}%`);
+      artistRef.current.style.setProperty('--scale', '1');
 
       cnt++;
 
       if (cnt % 15 === 0) {
         const bigSkew = Math.random() * 180 - 90;
-        ref.style.setProperty('--skew', `${bigSkew}deg`);
+        artistRef.current.style.setProperty('--skew', `${bigSkew}deg`);
       }
       if (cnt % 30 === 0) {
         const bigScale = 1 + Math.random() / 2;
-        ref.style.setProperty('--scale', `${bigScale}`);
+        artistRef.current.style.setProperty('--scale', `${bigScale}`);
       }
     }, 100);
 
     return () => {
-      clearInterval(interval);
+      clearInterval(glitchInterval);
+      document.removeEventListener('mousemove', mousemove);
     };
   }, []);
 
   return (
     <div className='base'>
+      <div ref={mouseRef} id='mouse-cursor'>
+        <div ref={mouseChildRef}></div>
+      </div>
+    
       <h1 className='artist' ref={artistRef} data-text='LUCY'>LUCY</h1>
 
       <div className='box'>
-        <span className='title left' onClick={() => toggleMenu('BM')}>Boogie Man</span>
+        <span data-cursor='title' className='title left' onClick={() => toggleMenu('BM')}>Boogie Man</span>
         <div className={`content${menu === 'BM' ? " show-ctt" : ""}${fadeOut ? " fade-out" : ""}`}>
           {menu === 'BM' && displayedText.map((a, i) => (
-            <span key={i}>{a}</span>
+            <span data-cursor='liryc' key={i}>{a}</span>
           ))}
         </div>
       </div>
       <div className='box'>
-        <span className='title right' onClick={() => toggleMenu('OTC')}>Over the Christmas</span>
+        <span data-cursor='title' className='title right' onClick={() => toggleMenu('OTC')}>Over the Christmas</span>
         <div className={`content${menu === 'OTC' ? " show-ctt" : ""}${fadeOut ? " fade-out" : ""}`}>
           {menu === 'OTC' && displayedText.map((a, i) => (
-            <span key={i}>{a}</span>
+            <span data-cursor='liryc' key={i}>{a}</span>
           ))}
         </div>
       </div>
